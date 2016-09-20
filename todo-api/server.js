cec4977e82bcde07501398068ff38c01d73a6a27
Shota Chakhvashvili <shota4ax@mail.ru>
2016-09-20 21:14:15 +0300

    Add /todos and /todos/:id

--- todo-api/server.js
+++ todo-api/server.js
@@ -2,11 +2,44 @@ var express = require('express');
 
 var app = express();
 
-
+var todos=[{
+    id:1,
+    description: 'Work hard!',
+    completed: false
+},{
+    id:2,
+    description: 'Go to market',
+    completed: false
+},{
+    id:3,
+    description: 'Go for a walk',
+    completed: true
+}];
 var PORT = process.env.PORT || 3000;
 app.get('/',function(req,res){
    res.send("Todo-api initialize"); 
 });
+
+//GET request /todos
+app.get('/todos',function(req,res){
+    res.json(todos);
+});
+//: after params of request
+app.get('/todos/:id',function(req,res){
+   var todoId =parseInt(req.params.id);
+    var matchTodo;
+    todos.forEach(function(todo){
+       if(todo.id === todoId){
+           matchTodo = todo;
+       }
+    });
+    if(matchTodo){
+        res.json(matchTodo.description);
+    }else{
+           res.status(404).send();
+       }
+});
+
 
 
 app.listen(PORT,function(){
